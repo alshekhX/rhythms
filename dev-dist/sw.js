25d6c8e2739b1fe5685aa2a486c9648e1c7d0cfe
alshekhX <60186591+alshekhX@users.noreply.github.com>
2024-06-18 04:43:05 -0700

    adddding sh

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.5rc4sv45u1g"
+    "revision": "0.i0bdl4qo2u8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
